369769bd163c99143ad3e58b8a7ca2cdfb7fcdc4
Roberto <robert.9524@gmail.com>
2020-07-29 11:49:28 -0400

    Added logos to the home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,27 @@
 import React from 'react';
+import reactLogo from '../image/reactlogo.png';
+import fccLogo from '../image/fcclogo.png';
 
 const Home = () => (
   <div className="Home">
     <h1 className="title">FCC - Front End Libraries</h1>
     <p className="lead">
-      Here are some apps built with React, use the navigation menu to explore
-      them
+      This project contains different apps built with React, use the navigation
+      menu to explore them
     </p>
     <p>Built as part of freeCodeCamp's Front End Libraries Certification</p>
+    <div className="home-logos">
+      <a href="http://reactjs.org" target="_blank" rel="noopener noreferrer">
+        <img src={reactLogo} alt="React logo" />
+      </a>
+      <a
+        href="https://www.freecodecamp.org/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <img src={fccLogo} alt="freeCodeCamp logo" />
+      </a>
+    </div>
   </div>
 );
 
--- src/components/common/NotFound.js
+++ src/components/common/NotFound.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
 const NotFound = () => (
-  <>
+  <div className="Home">
     <h1>404 - Not Found</h1>
     <p>The page you are trying to access does not exist.</p>
-  </>
+  </div>
 );
 
 export default NotFound;
